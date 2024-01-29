b166c67c5ffd6b755b626265435139ffd03292a8
cryptospider937 <mihir.mahakalkar@gmail.com>
2024-01-29 15:48:53 +0530

    feat: Movies list

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  console.log('Movies');
+  const { data, error, isFetching } = useGetMoviesQuery();
+  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
+
+  const numberOfMovies = lg ? 17 : 19;
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occured.';
+
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data} numberOfMovies={numberOfMovies} excludeFirst />
+    </div>
   );
 };
 
--- src/components/Siderbar/Sidebar.jsx
+++ src/components/Siderbar/Sidebar.jsx
@@ -4,25 +4,23 @@ import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 
 import useStyles from './styles';
+import { useGetGenresQuery } from '../../services/TMDB';
+import genreIcons from '../../assets/genres';
 
 const categories = [
   { label: 'Popular', value: 'popular' },
-  { label: 'Top Rted', value: 'top_rated' },
+  { label: 'Top Rated', value: 'topRated' },
   { label: 'Upcoming', value: 'upcoming' },
 
 ];
-const demoCategories = [
-  { label: 'Comedy', value: 'comedy' },
-  { label: 'Action', value: 'action' },
-  { label: 'Horror', value: 'horror' },
-  { label: 'Animation', value: 'animation' },
-];
+
 const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
 
 const Sidebar = ({ setMobileOpen }) => {
   const theme = useTheme();
   const classes = useStyles();
+  const { data, isFetching } = useGetGenresQuery();
 
   return (
     <>
@@ -39,6 +37,9 @@ const Sidebar = ({ setMobileOpen }) => {
         {categories.map(({ label, value }) => (
           <Link key={value} className={classes.links} to="/">
             <ListItemButton onClick={() => {}}>
+              <ListItemIcon>
+                <img src={genreIcons[label.toLowerCase()]} className={classes.genreImage} height={30} />
+              </ListItemIcon>
               <ListItemText primary={label} />
             </ListItemButton>
           </Link>
@@ -47,10 +48,17 @@ const Sidebar = ({ setMobileOpen }) => {
       <Divider />
       <List>
         <ListSubheader>Genres</ListSubheader>
-        {demoCategories.map(({ label, value }) => (
-          <Link key={value} className={classes.links} to="/">
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress size="4rem" />
+          </Box>
+        ) : data.genres.map(({ name, id }) => (
+          <Link key={id} className={classes.links} to="/">
             <ListItemButton onClick={() => {}}>
-              <ListItemText primary={label} />
+              <ListItemIcon>
+                <img src={genreIcons[name.toLowerCase()]} className={classes.genreImage} height={30} />
+              </ListItemIcon>
+              <ListItemText primary={name} />
             </ListItemButton>
           </Link>
         ))}
